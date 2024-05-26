f7770cf7506644d834a0becd5914348ee52e29ad
nand kishor kumar <91613478+nandkishor999@users.noreply.github.com>
2024-05-26 20:17:00 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { store } from './app/store';
-import './app.css';
+
 import { Provider } from 'react-redux';
 
 ReactDOM.render(
